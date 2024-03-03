c7c9f6dd2a19535527ed3a6f9f3a9e5eaf8a7855
yeisonagm <garciamonsefu@gmail.com>
2024-03-03 01:45:01 -0500

    Define la clase `ErrorMessage`, que permite personalizar los mensajes de error

---
+++ src/main/java/edu/unc/eventos/exception/ErrorMessage.java
@@ -0,0 +1,51 @@
+/**
+ * @file: ErrorMessage.java
+ * @author: (c)2024 Yeison García
+ * @created: Mar 03, 2024 03:32:04 AN
+ */
+package edu.unc.eventos.exception;
+
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.http.HttpStatus;
+
+import java.time.LocalDateTime;
+
+/**
+ * Clase que representa un mensaje de error personalizado.
+ * Excepción que se lanza cuando se realiza una operación ilegal.
+ */
+@Getter
+@Setter
+public class ErrorMessage {
+    /**
+     * Código de estado HTTP de la operación.
+     */
+    private int statusCode;
+    /**
+     * Fecha y hora en que se generó el error.
+     */
+    private LocalDateTime timestamp;
+    /**
+     * Mensaje descriptivo del error.
+     */
+    private String message;
+    /**
+     * Descripción detallada del error.
+     */
+    private String description;
+
+    /**
+     * Constructor de la clase ErrorMessage.
+     *
+     * @param statusCode  Código de estado HTTP asociado al error.
+     * @param message     Mensaje preciso del error.
+     * @param description Descripción detallada del error.
+     */
+    public ErrorMessage(HttpStatus statusCode, String message, String description) {
+        this.statusCode = statusCode.value();
+        this.timestamp = LocalDateTime.now();
+        this.message = message;
+        this.description = description;
+    }
+}
