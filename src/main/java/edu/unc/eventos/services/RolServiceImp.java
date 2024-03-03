708b4d77bf4222bd9585e2cbdd33271c24b066e8
AldoAndo <alfapemo@gmail.com>
2024-03-02 19:01:28 -0500

    Actualizar las clases ServiceImp de Local,PLato,Rol, Seguro

--- src/main/java/edu/unc/eventos/services/RolServiceImp.java
+++ src/main/java/edu/unc/eventos/services/RolServiceImp.java
@@ -1,28 +1,37 @@
 package edu.unc.eventos.services;
 
 import edu.unc.eventos.domain.Rol;
+import edu.unc.eventos.repositories.LocalRepository;
+import edu.unc.eventos.repositories.RolRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 import java.util.Optional;
 
+@Service
 public class RolServiceImp implements RolService{
+    @Autowired
+    private RolRepository rolRepository;
     @Override
     public List<Rol> getAllRol() {
-        return null;
+        return (List<Rol>)rolRepository.findAll() ;
     }
 
     @Override
+    @Transactional(readOnly = true)
     public Optional<Rol> getRolById(Long idRol) {
-        return Optional.empty();
+        return rolRepository.findById(idRol);
     }
 
     @Override
     public Rol saveRol(Rol rol) {
-        return null;
+        return rolRepository.save(rol);
     }
 
     @Override
     public void deleteRol(Long idRol) {
-
+        rolRepository.deleteById(idRol);
     }
 }
