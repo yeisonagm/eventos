6f9dd3aff60392f0a9612035b6b8de544da62cf0
Rocasari <rsalazarr20_1@unc.edu.pe>
2024-03-03 17:24:02 -0500

    Documenta las clases `EventoDTO` y `RolDTO`

--- src/main/java/edu/unc/eventos/dto/RolDTO.java
+++ src/main/java/edu/unc/eventos/dto/RolDTO.java
@@ -1,3 +1,8 @@
+/**
+ * @file: RolDTO.java
+ * @author: (c)2024 Roberto Salazar (rocasari)
+ * @created: Mar 03, 2024 05:22:48 PM
+ */
 package edu.unc.eventos.dto;
 
 import edu.unc.eventos.domain.Empleado;
@@ -5,9 +10,25 @@ import lombok.Data;
 
 import java.util.List;
 
+/**
+ * La clase RolDTO es un objeto de transferencia de datos (DTO) que se utiliza para representar información
+ * relacionada con roles en el sistema. A diferencia de la clase Rol, este DTO contiene solo los campos
+ * relevantes para la comunicación entre capas o servicios.
+ */
 @Data
 public class RolDTO {
+    /**
+     * El campo 'idRol' es el identificador único del rol.
+     */
     private Long idRol;
+
+    /**
+     * El campo 'nombre' representa el nombre del rol.
+     */
     private String nombre;
+
+    /**
+     * El campo 'empleados' es una lista de los empleados asociados a este rol.
+     */
     private List<Empleado> empleados;
 }
