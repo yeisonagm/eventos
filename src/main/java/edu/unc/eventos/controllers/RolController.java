3f9a36f6bcbb206c8fad5c392112e3ba9aac82b7
Rocasari <rsalazarr20_1@unc.edu.pe>
2024-03-04 00:13:43 -0500

    Crea el controlador para la clase `RolController`

--- src/main/java/edu/unc/eventos/controllers/RolController.java
+++ src/main/java/edu/unc/eventos/controllers/RolController.java
@@ -1,10 +1,115 @@
+/**
+ * @file: RolController.java
+ * @author: (c)2024 Roberto Salazar (rocasari)
+ * @created: Mar 04, 2024 00:04:45 AM
+ */
 package edu.unc.eventos.controllers;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import edu.unc.eventos.domain.Rol;
+import edu.unc.eventos.dto.RolDTO;
+import edu.unc.eventos.exception.EntityNotFoundException;
+import edu.unc.eventos.exception.IllegalOperationException;
+import edu.unc.eventos.services.RolService;
+import edu.unc.eventos.util.ApiResponse;
+import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping(value = "/api/roles", headers = "Api-Version=1")
 public class RolController {
-    
+
+    @Autowired
+    private RolService rolService;
+
+    @Autowired
+    private ModelMapper modelMapper;
+
+    /**
+     * Obtiene todos los roles existentes
+     *
+     * @return Lista de roles
+     */
+    @GetMapping
+    public ResponseEntity<?> getAll() {
+        List<Rol> roles = rolService.getAll();
+        if (roles == null || roles.isEmpty()) {
+            return ResponseEntity.noContent().build();
+        } else {
+            List<RolDTO> rolesDTOs = roles.stream()
+                    .map(rol -> modelMapper.map(rol, RolDTO.class))
+                    .collect(Collectors.toList());
+
+            ApiResponse<List<RolDTO>> response = new ApiResponse<>(true, "Lista de roles", rolesDTOs);
+            return ResponseEntity.ok(response);
+        }
+    }
+
+    /**
+     * Obtiene un rol por su identificador
+     *
+     * @param id Idetificador del rol
+     * @return Rol
+     */
+    @GetMapping("/{id}")
+    public ResponseEntity<?> getById(@PathVariable Long id) {
+        Rol rol = rolService.getRolById(id);
+        RolDTO rolDTO = modelMapper.map(rol, RolDTO.class);
+        ApiResponse<RolDTO> response = new ApiResponse<>(true, "Rol", rolDTO);
+        return ResponseEntity.ok(response);
+    }
+
+    /**
+     * Crear un nuevo Rol
+     *
+     * @param rolDTO Datos del nuevo Rol
+     * @return Respuesta indicando la operación con éxito
+     * @throws IllegalOperationException Si hay una operación ilegal
+     */
+    @PostMapping
+    public ResponseEntity<ApiResponse<RolDTO>> create(@RequestBody RolDTO rolDTO) throws IllegalOperationException {
+        Rol rol = modelMapper.map(rolDTO, Rol.class);
+        rolService.save(rol);
+        RolDTO createdDTO = modelMapper.map(rol, RolDTO.class);
+        ApiResponse<RolDTO> response = new ApiResponse<>(true, "Rol creado con éxito", createdDTO);
+        return ResponseEntity.status(HttpStatus.CREATED).body(response);
+    }
+
+    /**
+     * Actualizar un Rol por su ID
+     *
+     * @param id     Idetificador del rol
+     * @param rolDTO Nuevos datos del Rol
+     * @return Respuesta indicando la operación con éxito
+     * @throws EntityNotFoundException   Si el Rol no existe
+     * @throws IllegalOperationException Si hay una operación ilegal
+     */
+    @PutMapping("/{id}")
+    public ResponseEntity<ApiResponse<RolDTO>> update(@PathVariable Long id, @RequestBody RolDTO rolDTO) throws EntityNotFoundException, IllegalOperationException {
+        Rol rol = modelMapper.map(rolDTO, Rol.class);
+        rolService.update(id, rol);
+        RolDTO updateDTO = modelMapper.map(rol, RolDTO.class);
+        ApiResponse<RolDTO> response = new ApiResponse<>(true, "Rol actualizado", updateDTO);
+        return ResponseEntity.ok(response);
+    }
+
+    /**
+     * Eliminar un Rol por su ID
+     *
+     * @param id Idetificador del rol
+     * @return Respuesta indicando la operación con éxito
+     * @throws EntityNotFoundException   Si el Rol no existe
+     * @throws IllegalOperationException Si hay una operación ilegal
+     */
+    @DeleteMapping("/{id}")
+    public ResponseEntity<?> delete(@PathVariable Long id) throws EntityNotFoundException, IllegalOperationException {
+        rolService.delete(id);
+        ApiResponse<?> response = new ApiResponse<>(true, "Rol eliminado con éxito", null);
+        return ResponseEntity.status(HttpStatus.OK).body(response);
+    }
 }
