8a3cbf84b7be78b895789c23ff516aa8e732b5fa
yeisonagm <garciamonsefu@gmail.com>
2024-03-04 00:08:19 -0500

    Corrige y agrega todos los métodos de `DecoracionController`

--- src/main/java/edu/unc/eventos/controllers/DecoracionController.java
+++ src/main/java/edu/unc/eventos/controllers/DecoracionController.java
@@ -1,24 +1,23 @@
 /**
  * @file: ApplicationConfig.java
  * @author: (c)2024 Yeison García
- * @created: Mar 03, 2024 07:20:18 PM
+ * @created: Mar 03, 2024 11:30:18 PM
  */
 package edu.unc.eventos.controllers;
 
 import edu.unc.eventos.domain.Decoracion;
 import edu.unc.eventos.dto.DecoracionDTO;
+import edu.unc.eventos.exception.EntityNotFoundException;
+import edu.unc.eventos.exception.IllegalOperationException;
 import edu.unc.eventos.services.DecoracionService;
 import edu.unc.eventos.util.ApiResponse;
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
-import java.util.Optional;
 import java.util.stream.Collectors;
 
 /**
@@ -43,14 +42,13 @@ public class DecoracionController {
      */
     @GetMapping
     public ResponseEntity<?> getAll() {
-        List<Decoracion> decoraciones = decoracionService.getAllDecoraciones();
+        List<Decoracion> decoraciones = decoracionService.getAll();
         if (decoraciones == null || decoraciones.isEmpty()) {
             return ResponseEntity.noContent().build();
         } else {
             List<DecoracionDTO> decoracionesDTOs = decoraciones.stream()
                     .map(decoracion -> modelMapper.map(decoracion, DecoracionDTO.class))
                     .collect(Collectors.toList());
-
             ApiResponse<List<DecoracionDTO>> response = new ApiResponse<>(true, "Lista de decoraciones", decoracionesDTOs);
             return ResponseEntity.ok(response);
         }
@@ -64,13 +62,59 @@ public class DecoracionController {
      */
     @GetMapping("/{id}")
     public ResponseEntity<?> getById(@PathVariable Long id) {
-        Optional<Decoracion> decoracionOpt = decoracionService.getDecoracionById(id);
-        if (decoracionOpt.isEmpty()) {
-            return ResponseEntity.noContent().build();
-        }
-        DecoracionDTO decoracionDTO = modelMapper.map(decoracionOpt.get(), DecoracionDTO.class);
+        Decoracion decoracion = decoracionService.getById(id);
+        DecoracionDTO decoracionDTO = modelMapper.map(decoracion, DecoracionDTO.class);
         ApiResponse<DecoracionDTO> response = new ApiResponse<>(true, "Decoración", decoracionDTO);
         return ResponseEntity.ok(response);
+    }
+
+    /**
+     * Crea una nueva decoración
+     *
+     * @param decoracionDTO Datos nuevos de la decoración
+     * @return Decoración creada
+     * @throws IllegalOperationException Sí se genera una operación ilegal
+     */
+    @PostMapping
+    public ResponseEntity<?> save(@RequestBody DecoracionDTO decoracionDTO) throws IllegalOperationException {
+        Decoracion decoracion = modelMapper.map(decoracionDTO, Decoracion.class);
+        decoracionService.save(decoracion);
+        DecoracionDTO saveDTO = modelMapper.map(decoracion, DecoracionDTO.class);
+        ApiResponse<DecoracionDTO> response = new ApiResponse<>(true, "Decoración guardada", saveDTO);
+        return ResponseEntity.status(HttpStatus.CREATED).body(response);
+    }
+
+    /**
+     * Actualiza una decoración existente
+     *
+     * @param id            Identificador de la decoración
+     * @param decoracionDTO Datos actualizados de la decoración
+     * @return Decoración actualizada
+     * @throws EntityNotFoundException   Si la decoración no existe
+     * @throws IllegalOperationException Sí hay una operación ilegal
+     */
+    @PutMapping("/{id}")
+    public ResponseEntity<ApiResponse<DecoracionDTO>> update(@PathVariable Long id, @RequestBody DecoracionDTO decoracionDTO) throws EntityNotFoundException, IllegalOperationException {
+        Decoracion decoracion = modelMapper.map(decoracionDTO, Decoracion.class);
+        decoracionService.update(id, decoracion);
+        DecoracionDTO updateDTO = modelMapper.map(decoracion, DecoracionDTO.class);
+        ApiResponse<DecoracionDTO> response = new ApiResponse<>(true, "Decoración actualizada", updateDTO);
+        return ResponseEntity.status(HttpStatus.OK).body(response);
+    }
+
+    /**
+     * Elimina una decoración existente por su Id
+     *
+     * @param id Identificador de la decoración
+     * @return Respuesta indicando el éxito de la operación
+     * @throws EntityNotFoundException   Si la decoración no existe
+     * @throws IllegalOperationException Sí hay una operación ilegal
+     */
+    @DeleteMapping("/{id}")
+    public ResponseEntity<?> delete(@PathVariable Long id) throws EntityNotFoundException, IllegalOperationException {
+        decoracionService.delete(id);
+        ApiResponse<?> response = new ApiResponse<>(true, "Rol eliminado con éxito", null);
+        return ResponseEntity.status(HttpStatus.OK).body(response);
     }
 
 }
