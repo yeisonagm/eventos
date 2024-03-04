0dabfa20681aa4ec06fff470ebd62d511bac1df4
Rocasari <rsalazarr20_1@unc.edu.pe>
2024-03-04 16:19:38 -0500

    Agrega la documentación en las clases `RolService` y `EventoService`

--- src/main/java/edu/unc/eventos/services/EventoService.java
+++ src/main/java/edu/unc/eventos/services/EventoService.java
@@ -2,6 +2,8 @@
  * @file: EventoService.java
  * @author: (c)2024 Roberto Salazar (rocasari)
  * @created: Mar 03, 2024 10:39:39 PM
+ * @description: Interfaz que define los servicios disponibles para la gestión de eventos en el sistema.
+ * Proporciona métodos para la recuperación, creación, actualización y eliminación de eventos.
  */
 package edu.unc.eventos.services;
 
@@ -13,13 +15,48 @@ import java.util.List;
 import java.util.Optional;
 
 public interface EventoService {
+    /**
+     * Recupera todos los eventos registrados.
+     *
+     * @return Lista de objetos Evento.
+     */
     List<Evento> getAll();
 
+    /**
+     * Recupera un evento específico según su ID
+     *
+     * @param idEvento Idetificador único del evento.
+     * @return
+     * @throws EntityNotFoundException Si no se encuentra ningún evento con el identificador especificado.
+     */
     Evento getEventoById(Long idEvento) throws EntityNotFoundException;
 
+    /**
+     * Guarda un nuevo local
+     *
+     * @param evento El objeto Evento que se va a guardar
+     * @return El objeto Evento guardado
+     * @throws IllegalOperationException Si hay algún problema con los datos del evento que impide su guardado.
+     */
     Evento save(Evento evento) throws IllegalOperationException;
 
+    /**
+     * Actualiza un evento axistente.
+     *
+     * @param idEvento El identificador único del evento que se va a actualizar
+     * @param evento El objeto Evento con los nuevos datos
+     * @return El objeto Evento actualizado
+     * @throws EntityNotFoundException Si no se encuentra ningún evento con el identificador especificado.
+     * @throws IllegalOperationException Si hay algún problema con los datos del evento que impide su actualización.
+     */
     Evento update(Long idEvento, Evento evento) throws EntityNotFoundException, IllegalOperationException;
 
+    /**
+     * Elimina un local del sistema según su identificador único
+     *
+     * @param idEvento El identificador único del evento que se va a eliminar
+     * @throws EntityNotFoundException Si no se encuentra ningún evento con el identificador especificado.
+     * @throws IllegalOperationException Si hay algún problema que impide la eliminación del evento.
+     */
     void delete(Long idEvento) throws EntityNotFoundException, IllegalOperationException;
 }
--- src/main/java/edu/unc/eventos/services/RolService.java
+++ src/main/java/edu/unc/eventos/services/RolService.java
@@ -2,6 +2,8 @@
  * @file: RolService.java
  * @author: (c)2024 Roberto Salazar (rocasari)
  * @created: Mar 03, 2024 10:32:12 PM
+ * @description: Interfaz que define los servicios disponibles para la gestión de roles en el sistema.
+ * Proporciona métodos para la recuperación, creación, actualización y eliminación de roles.
  */
 package edu.unc.eventos.services;
 
@@ -10,17 +12,50 @@ import edu.unc.eventos.exception.EntityNotFoundException;
 import edu.unc.eventos.exception.IllegalOperationException;
 
 import java.util.List;
-import java.util.Optional;
 
 public interface RolService {
+    /**
+     * Recupera todos los roles registrados en el sistema.
+     *
+     * @return Lista de objetos Rol.
+     */
     List<Rol> getAll();
 
+    /**
+     * Recupera un rol específico según su ID.
+     *
+     * @param idRol Identificador único del rol.
+     * @return Objeto Rol correspondiente al ID proporcionado.
+     * @throws EntityNotFoundException Si no se encuentra ningún rol con el identificador especificado.
+     */
     Rol getRolById(Long idRol) throws EntityNotFoundException;
 
+    /**
+     * Guarda un nuevo rol en el sistema.
+     *
+     * @param rol Objeto Rol que se va a guardar.
+     * @return Objeto Rol guardado.
+     * @throws IllegalOperationException Si hay algún problema con los datos del rol que impide su guardado.
+     */
     Rol save(Rol rol) throws IllegalOperationException;
 
+    /**
+     * Actualiza un rol existente en el sistema.
+     *
+     * @param idRol Identificador único del rol que se va a actualizar.
+     * @param rol Objeto Rol con los nuevos datos.
+     * @return Objeto Rol actualizado.
+     * @throws EntityNotFoundException Si no se encuentra ningún rol con el identificador especificado.
+     * @throws IllegalOperationException Si hay algún problema con los datos del rol que impide su actualización.
+     */
     Rol update(Long idRol, Rol rol) throws EntityNotFoundException, IllegalOperationException;
 
+    /**
+     * Elimina un rol del sistema según su identificador único.
+     *
+     * @param idRol Identificador único del rol que se va a eliminar.
+     * @throws EntityNotFoundException Si no se encuentra ningún rol con el identificador especificado.
+     * @throws IllegalOperationException Si hay algún problema que impide la eliminación del rol.
+     */
     void delete(Long idRol) throws EntityNotFoundException, IllegalOperationException;
-
 }
