94fc6d581a9090ad8562f64f8bde7ea81c4ef910
Peter Vásquez <pavs3013@gmail.com>
2024-03-04 23:03:48 -0500

    Creamos la lógica de negocio compleja de asignarle un plato a un evento.

--- src/main/java/edu/unc/eventos/controllers/EventoController.java
+++ src/main/java/edu/unc/eventos/controllers/EventoController.java
@@ -6,11 +6,14 @@
 package edu.unc.eventos.controllers;
 
 import edu.unc.eventos.domain.Evento;
+import edu.unc.eventos.domain.Local;
 import edu.unc.eventos.dto.EventoDTO;
 import edu.unc.eventos.exception.EntityNotFoundException;
 import edu.unc.eventos.exception.IllegalOperationException;
 import edu.unc.eventos.services.EventoService;
 import edu.unc.eventos.util.ApiResponse;
+import jakarta.validation.ConstraintViolation;
+import jakarta.validation.Validator;
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -19,6 +22,7 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 import java.util.Optional;
+import java.util.Set;
 import java.util.stream.Collectors;
 
 @RestController
@@ -30,6 +34,9 @@ class EventoController {
 
     @Autowired
     private ModelMapper modelMapper;
+
+    @Autowired
+    private Validator validator;
 
     /**
      * Obtiene todos los eventos existentes
@@ -97,6 +104,25 @@ class EventoController {
         EventoDTO updateDTO = modelMapper.map(evento, EventoDTO.class);
         ApiResponse<EventoDTO> response = new ApiResponse<>(true, "Evento actualizado", updateDTO);
         return ResponseEntity.status(HttpStatus.OK).body(response);
+    }
+
+    /**
+     * Agrega un plato a un evento existente mediante una solicitud PATCH.
+     *
+     * Este método PATCH permite asociar un plato existente a un evento existente mediante sus identificadores.
+     * Recibe el ID del evento y el ID del plato que se desea asociar como parámetros de la URL y la solicitud.
+     * Llama al servicio de eventos para realizar la operación de asociación.
+     * Si la operación se realiza con éxito, devuelve una respuesta con estado OK y un mensaje indicando que el plato ha sido agregado al evento correctamente.
+     *
+     * @param idEvento El ID del evento al que se desea añadir el plato.
+     * @param idPlato El ID del plato que se desea asociar al evento.
+     * @return ResponseEntity que contiene una respuesta con estado OK y un mensaje indicando que el plato ha sido agregado al evento correctamente.
+     * @throws IllegalOperationException Si ocurre una operación ilegal durante la asociación del plato al evento.
+     */
+    @PatchMapping("/{idEvento}/addPlato")
+    public ResponseEntity<String> addplato (@PathVariable Long idEvento, @RequestParam Long idPlato) throws IllegalOperationException {
+        eventoService.addPlato(idEvento, idPlato);
+        return ResponseEntity.ok("Plato agregado al evento correctamente");
     }
 
     /**
--- src/main/java/edu/unc/eventos/services/EventoServiceImp.java
+++ src/main/java/edu/unc/eventos/services/EventoServiceImp.java
@@ -7,10 +7,12 @@ package edu.unc.eventos.services;
 
 import edu.unc.eventos.domain.Evento;
 import edu.unc.eventos.domain.Local;
+import edu.unc.eventos.domain.Plato;
 import edu.unc.eventos.domain.Rol;
 import edu.unc.eventos.exception.EntityNotFoundException;
 import edu.unc.eventos.exception.IllegalOperationException;
 import edu.unc.eventos.repositories.EventoRepository;
+import edu.unc.eventos.repositories.PlatoRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -27,6 +29,10 @@ public class EventoServiceImp implements EventoService {
 
     @Autowired
     private EventoRepository eventoRepository;
+
+    @Autowired
+    private PlatoRepository platoRepository;
+
 
     /**
      * Este método devuelve una lista de todos los eventos
@@ -146,5 +152,37 @@ public class EventoServiceImp implements EventoService {
         LocalDate fechaEvento = fecha.toInstant().atZone(ZoneId.systemDefault()).toLocalDate();
         List<Evento> eventosEnMismoDiaYLocal = eventoRepository.findByLocalAndFecha(local, fechaEvento);
         return !eventosEnMismoDiaYLocal.isEmpty();
+    }
+
+    /**
+     * Agrega un plato a un evento existente.
+     *
+     * Este método permite asociar un plato existente a un evento existente mediante sus identificadores.
+     * Recibe el ID del evento y el ID del plato que se desea asociar.
+     * Busca el evento y el plato correspondientes en la base de datos utilizando los repositorios respectivos.
+     * Si el evento o el plato no se encuentran en la base de datos, lanza una excepción de tipo EntityNotFoundException.
+     * Si el plato ya está asociado al evento, lanza una excepción de tipo IllegalOperationException.
+     * Si la operación se realiza con éxito, guarda el evento actualizado en la base de datos y lo devuelve.
+     *
+     * @param idEvento El ID del evento al que se desea añadir el plato.
+     * @param idPlato El ID del plato que se desea asociar al evento.
+     * @return El evento actualizado con el plato añadido.
+     * @throws EntityNotFoundException Si el evento o el plato con los IDs especificados no se encuentran en la base de datos.
+     * @throws IllegalOperationException Si el plato ya está asociado al evento.
+     */
+    @Override
+    public Evento addPlato(Long idEvento, Long idPlato) throws EntityNotFoundException, IllegalOperationException {
+        Evento evento = eventoRepository.findById(idEvento)
+                .orElseThrow(() -> new EntityNotFoundException("El Evento no se ha encontrado"));
+
+        Plato plato = platoRepository.findById(idPlato)
+                .orElseThrow(() -> new EntityNotFoundException("El Plato no se ha encontrado"));
+
+        if (evento.getPlatos().contains(plato)) {
+            throw new IllegalOperationException("El plato ya está agregado al evento");
+        }
+        evento.getPlatos().add(plato);
+        eventoRepository.save(evento);
+        return evento;
     }
 }
